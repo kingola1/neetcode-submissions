1130f748ec47df60149cdd0002fee0ba4f57e26d
David Aremu <66888450+kingola1@users.noreply.github.com>
2026-09-23 23:46:43 +0100

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[]} height
+     * @return {number}
+     */
+    trap(height) {
+        const n = height.length;
+        if (n === 0) return 0;
+
+        const leftMax = new Array(n);
+        const rightMax = new Array(n);
+
+        leftMax[0] = height[0];
+        for (let i = 1; i < n; i++) {
+            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+        }
+
+        rightMax[n - 1] = height[n - 1];
+        for (let i = n - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
+        }
+
+        let res = 0;
+        for (let i = 0; i < n; i++) {
+            res += Math.min(leftMax[i], rightMax[i]) - height[i];
+        }
+        return res;
+    }
+}
